a4585cb2f8719639d8a6fee810411e1b3c418a0c
Your Name <you@example.com>
2019-05-14 14:42:43 +0700

    commit awal

---
+++ PolymorphismPrinter/Cannon.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismPrinter
+{
+    class Cannon : PrinterWindows
+    {
+        public override void Show()
+        {
+            Console.WriteLine("Cannon display dimension: 9.5*12");
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine("Cannon printer printing....");
+        }
+    }
+}
---
+++ PolymorphismPrinter/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismPrinter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrinterWindows printer;
+
+            Console.WriteLine("Pilih Printer");
+            Console.WriteLine("1. Epson");
+            Console.WriteLine("2. Cannoc");
+            Console.WriteLine("3. LaserJet\n");
+
+            Console.Write("Nomor Printer [1..3]: ");
+            int noPrinter = Convert.ToInt32(Console.ReadLine());
+
+            if (noPrinter==1) {
+                printer = new Epson();
+            }else if (noPrinter == 2){
+                printer = new Cannon();
+            }else {
+                printer = new LaserJet();
+            }
+
+            printer.Show();
+            printer.Print();
+
+            Console.ReadKey();
+        }
+    }
+}
